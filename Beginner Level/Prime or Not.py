3326477639e2505302d0f3268a8a7378fc531001
PrasanthJack <37337258+PrasanthJack@users.noreply.github.com>
2018-03-20 10:58:19 +0530

    Create Prime or Not.py

---
+++ Beginner Level/Prime or Not.py
@@ -0,0 +1,13 @@
+num = int(input())
+
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            print('No')
+            break
+    else:
+        print('Yes')
+
+
+else:
+    print('NO')
